8e1171de261ac180ecf30cab1780f1e746a9146e
SEUNGJUN JEONG <jsj9620@naver.com>
2022-05-04 22:40:29 +0900

    print dice img & add app, dice components

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// App 컴포넌트
+
+import Dice from './Dice';
+
+function App() {
+    return (
+        <div>
+            <Dice />
+        </div>
+    )
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 import ReactDOM from 'react-dom/client';
+import App from './App';
 
 // root id 가져오고, render 메소드로 집어넣기
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <>
-    <h2>Hello</h2>
-    <h2>React!</h2>
-  </>
-  
-); // render 메소드로 html 태그 제작
+root.render(<App />); // render 메소드로 html 태그 제작
 
