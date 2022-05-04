e4be42a29a6520b73efc4befd97eb3c37422ad19
SEUNGJUN JEONG <jsj9620@naver.com>
2022-05-04 22:25:33 +0900

    add comments of index file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import ReactDOM from 'react-dom/client';
 
+// root id 가져오고, render 메소드로 집어넣기
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <h1>Hello React!</h1>
-);
+); // render 메소드로 html 태그 제작
 
