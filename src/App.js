5bc5f85c420007acca585a4a2f7001ebdc1ca0e3
SEUNGJUN JEONG <jsj9620@naver.com>
2022-05-05 00:08:46 +0900

    use children props

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
     return (
         <div>
             <div>
-                <Button text = "던지기" />
-                <Button text = "처음부터" />
+                <Button>던지기</Button>
+                <Button>처음부터</Button>
             </div>
             <Dice color = "red" num = {1} />
         </div>
