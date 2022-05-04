d85e6a64c23a7ee87af1c3e2f58efb8fa1f67ffa
SEUNGJUN JEONG <jsj9620@naver.com>
2022-05-04 22:29:56 +0900

    use fragment to remove unnecessary div tag

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 // root id 가져오고, render 메소드로 집어넣기
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <h1>Hello React!</h1>
+  <>
+    <h2>Hello</h2>
+    <h2>React!</h2>
+  </>
+  
 ); // render 메소드로 html 태그 제작
 
