d9734a1eb717ee9e6447c3b127c745e5abace625
SEUNGJUN JEONG <jsj9620@naver.com>
2022-05-05 00:19:33 +0900

    add dice roll function, use state to change num

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 // App 컴포넌트
+// State: react가 알아서 화면을 새로 렌더링
 
+import { useState } from 'react';
 import Button from './Button';
 import Dice from './Dice';
 
 function App() {
+    // distructuring 사용 / parameter로 초기값 전달받고 배열형태로 요소 두개 return
+    // 첫 요소는 state값 - 현재 변수의 값을 나타냄 / 두번째 요소는 setter 함수 - 함수를 호출할 때 parameter로 전달하는 값으로 state값이 변경됨
+    const [num, setNum] = useState(1);
+
+    const handleRollClick = () => { // 주사위 던지기
+        setNum(3);
+    }
     return (
         <div>
             <div>
-                <Button>던지기</Button>
+                <Button onClick = {handleRollClick}>던지기</Button>
                 <Button>처음부터</Button>
             </div>
-            <Dice color = "red" num = {1} />
+            <Dice color = "red" num = {num} />
         </div>
     )
 }
--- src/Button.js
+++ src/Button.js
@@ -1,8 +1,9 @@
 // children: 컴포넌트의 자식들을 값으로 가짐
 // 보여지기만 하는 값을 다룰 때는 어떠한 prop을 만드는 거보다는 children이 더 효과적 
 
-function Button({ children }) {
-    return <button>{children}</button>;
+function Button({ children, onClick }) {
+    // onClick prop 주고, 속성까지 주기
+    return <button onClick={onClick}>{children}</button>;
 }
 
 export default Button;
