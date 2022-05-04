802b73741117d3393b3d5eeced34eef0135eb3ab
SEUNGJUN JEONG <jsj9620@naver.com>
2022-05-05 00:34:54 +0900

    add dice history function

--- src/App.js
+++ src/App.js
@@ -14,16 +14,19 @@ function App() {
     // 첫 요소는 state값 - 현재 변수의 값을 나타냄 / 두번째 요소는 setter 함수 - 함수를 호출할 때 parameter로 전달하는 값으로 state값이 변경됨
     const [num, setNum] = useState(1); // 주사위 초기값 1
     const [sum, setSum] = useState(0); // 점수 합계
+    const [gameHistory, setGameHistory] = useState([]); // 배열 사용해 주사위 기록 나타냄, 초기값은 빈 배열
 
     const handleRollClick = () => { // 주사위 던지기
         const nextNum = random(6);
         setNum(nextNum); // 다음 숫자 랜덤
         setSum(sum + nextNum);
+        setGameHistory([...gameHistory, nextNum]);
     }
 
     const handleClearClick = () => { // 주사위 초기화
         setNum(1);
         setSum(0);
+        setGameHistory(0);
     }
     return (
         <div>
@@ -33,9 +36,11 @@ function App() {
             </div>
             <div>
                 <h2>나</h2>
-                <Dice color="red" num={num} />
+                    <Dice color="red" num={num} />
                 <h2>총점</h2>
-                <p>{sum}</p>
+                    <p>{sum}</p>
+                <h2>기록</h2>
+                    <p>{gameHistory.join(', ')}</p>
             </div>
         </div>
     );
