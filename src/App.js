d2589c88aa159ad438aec6eb0aa331bdd9b409ff
SEUNGJUN JEONG <jsj9620@naver.com>
2022-05-05 00:07:24 +0900

    add button component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 // App 컴포넌트
 
+import Button from './Button';
 import Dice from './Dice';
 
 function App() {
     return (
         <div>
+            <div>
+                <Button text = "던지기" />
+                <Button text = "처음부터" />
+            </div>
             <Dice color = "red" num = {1} />
         </div>
     )
---
+++ src/Button.js
@@ -0,0 +1,8 @@
+// children: 컴포넌트의 자식들을 값으로 가짐
+// 보여지기만 하는 값을 다룰 때는 어떠한 prop을 만드는 거보다는 children이 더 효과적 
+
+function Button({text}) {
+    return <button>{text}</button>;
+}
+
+export default Button;
