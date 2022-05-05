627fb0b49b1af0177d72713cefa124a92f1935a1
SEUNGJUN JEONG <jsj9620@naver.com>
2022-05-05 13:42:39 +0900

    use css file instead of inline style

--- src/Button.js
+++ src/Button.js
@@ -1,33 +1,12 @@
 // children: 컴포넌트의 자식들을 값으로 가짐
 // 보여지기만 하는 값을 다룰 때는 어떠한 prop을 만드는 거보다는 children이 더 효과적 
+import './Button.css';
 
-const baseButtonStyle = {
-    padding: '14px 27px',
-    borderRadius: '30px',
-    outline: 'none',
-    cursor: 'pointer',
-    fontSize: '17px',
-};
-
-const blueButtonStyle = {
-    ...baseButtonStyle, //spread 문법 사용해 공통적인 부분 최소화
-    backgroundColor: 'rgba(0, 89, 255, 0.2)',
-    border: 'solid 1px #7090ff',
-    color: '#7090ff',
-};
-
-const redButtonStyle = {
-    ...baseButtonStyle,
-    backgroundColor: 'rgba(255, 78, 78, 0.2)',
-    border: 'solid 1px #ff4664',
-    color: '#ff4664',
-};
-
-function Button({ children, onClick, color }) { //color는 app에서 전달됨
+function Button({ children, onClick, color = 'blue' }) { //color는 app에서 전달됨
     // onClick prop 주고, 속성까지 주기
-    const style = color === 'red' ? redButtonStyle : blueButtonStyle;
+    const classNames = `Button ${color}`; //class명 추가할 때 빈 공백 필요
     return (
-    <button style={style} onClick={onClick}>
+    <button className={classNames} onClick={onClick}>
         {children}
     </button>
     );
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css';
 
 // root id 가져오고, render 메소드로 집어넣기
 const root = ReactDOM.createRoot(document.getElementById('root'));
